f0374628face014bf7048da2ca15cef9f78c60ed
Owsap <owsap@mail.com>
2021-12-02 01:22:01 -0100

    Revert "Added support for scrollbar."
    This reverts commit f10326a006f6f008b8f54c895ed5efbd7aa1a3a3.

--- 2.Client/root/uichestdropinfo.py
+++ 2.Client/root/uichestdropinfo.py
@@ -1,6 +1,4 @@
-# Author: blackdragonx61
-# Date: 28.11.21
-
+#blackdragonx61 (28.11.21)
 import ui
 import item
 import uiToolTip
@@ -8,53 +6,51 @@ import uiToolTip
 class ChestDropInfoWindow(ui.ScriptWindow):
 	DROP_SLOT_SIZE = 5 * 8
 
-	def __init__(self):
+	def __init__(self) :
 		ui.ScriptWindow.__init__(self)
-		self.IsLoaded = False
-
+		self.isLoaded = False
 		self.ItemVnum = -1
 		self.PageCount = 0
-		self.ScrollPos = 0
-
+		self.CurrentPage = 0
 		self.DropDict = dict()
 		self.MainItemSlot = None
 		self.DropItemSlot = None
-		self.ScrollBar = None
+		self.prevButton = None
+		self.nextButton = None
+		self.currentPageText = None
+		self.tooltipitem = uiToolTip.ItemToolTip()
 
-		self.ToolTipItem = uiToolTip.ItemToolTip()
-
-	def __del__(self):
+	def __del__(self) :
 		ui.ScriptWindow.__del__(self)
-		self.ScrollPos = 0
-
 		self.DropDict = None
 		self.MainItemSlot = None
 		self.DropItemSlot = None
-		self.ScrollBar = None
-
-		self.ToolTipItem = None
-
+		self.prevButton = None
+		self.nextButton = None
+		self.currentPageText = None
+		self.tooltipitem = None
+		
 	def __LoadWindow(self):
-		if self.IsLoaded:
+		if self.isLoaded:
 			return
 
-		self.IsLoaded = True
+		self.isLoaded = True
 
-		# Script
+		# script
 		try:
 			self.__LoadScript("UIScript/ChestDropInfoWindow.py")
 		except:
 			import exception
 			exception.Abort("ChestDropInfoWindow.__LoadWindow.__LoadScript")
 
-		# Object
+		# object
 		try:
 			self.__BindObject()
 		except:
 			import exception
 			exception.Abort("ChestDropInfoWindow.__LoadWindow.__BindObject")
 
-		# Event
+		# event
 		try:
 			self.__BindEvent()
 		except:
@@ -66,32 +62,41 @@ class ChestDropInfoWindow(ui.ScriptWindow):
 		pyScrLoader.LoadScriptFile(self, fileName)
 
 	def __BindObject(self):
-		self.MainItemSlot = self.GetChild("main_item_slot")
-		self.DropItemSlot = self.GetChild("drop_item_slot")
-		self.ScrollBar = self.GetChild("scroll_bar")
+		self.MainItemSlot		= self.GetChild("main_item_slot")
+		self.DropItemSlot		= self.GetChild("drop_item_slot")
+		self.prevButton			= self.GetChild("prev_button")
+		self.nextButton			= self.GetChild("next_button")
+		self.currentPageText	= self.GetChild("CurrentPage")
 
 	def __BindEvent(self):
 		self.GetChild("board").SetCloseEvent(ui.__mem_func__(self.Close))
-
+		
 		self.MainItemSlot.SetOverInItemEvent(ui.__mem_func__(self.OverInMainItemSlot))
 		self.MainItemSlot.SetOverOutItemEvent(ui.__mem_func__(self.OverOutItem))
 
 		self.DropItemSlot.SetOverInItemEvent(ui.__mem_func__(self.OverInDropItemSlot))
 		self.DropItemSlot.SetOverOutItemEvent(ui.__mem_func__(self.OverOutItem))
 
-		self.ScrollBar.SetMiddleBarSize(0.5)
-		self.ScrollBar.SetScrollEvent(self.OnScroll)
-
+		self.prevButton.SetEvent(ui.__mem_func__(self.SetPage), -1)
+		self.nextButton.SetEvent(ui.__mem_func__(self.SetPage), +1)
+	
 	def UpdateItems(self):
 		for i in range(ChestDropInfoWindow.DROP_SLOT_SIZE):
 			self.DropItemSlot.ClearSlot(i)
 
-		if self.ScrollPos in self.DropDict:
-			for pos in self.DropDict[self.ScrollPos]:
-					self.DropItemSlot.SetItemSlot(pos, self.DropDict[self.ScrollPos][pos])
-
+		if self.CurrentPage in self.DropDict:
+			for pos in self.DropDict[self.CurrentPage]:
+				self.DropItemSlot.SetItemSlot(pos, self.DropDict[self.CurrentPage][pos])
+		
 		self.DropItemSlot.RefreshSlot()
-
+	
+	def SetPage(self, page):
+		nextpage = page + self.CurrentPage
+		if 0 <= nextpage <= self.PageCount:
+			self.CurrentPage = nextpage
+			self.currentPageText.SetText(str(self.CurrentPage + 1))
+			self.UpdateItems()
+	
 	def SetUp(self, itemVnum):
 		self.ItemVnum = itemVnum
 		self.MainItemSlot.SetItemSlot(0, self.ItemVnum, 0)
@@ -106,56 +111,38 @@ class ChestDropInfoWindow(ui.ScriptWindow):
 		for page, pos, vnum in DropList:
 			self.DropDict[page][pos] = vnum
 
-		self.ScrollPos = 0
-		if self.PageCount > 0:
-			self.ScrollBar.Show()
-			self.DropItemSlot.SetPosition(20, 90)
-		else:
-			self.DropItemSlot.SetPosition(20 + 10, 90)
-			self.ScrollBar.Hide()
-
-		self.UpdateItems()
-
-	def Open(self, itemVnum = 50254):
+		self.CurrentPage = 0
+		self.SetPage(0)
+	
+	def Open(self, itemVnum):
 		if self.IsShow():
 			return
 
 		self.__LoadWindow()
 		self.SetUp(itemVnum)
-
+		
 		self.SetCenterPosition()
 		self.SetTop()
 		self.Show()
-
+	
 	def OverInMainItemSlot(self, slotIndex):
-		if self.ToolTipItem:
-			self.ToolTipItem.SetItemToolTip(self.ItemVnum)
+		if self.tooltipitem:
+			self.tooltipitem.SetItemToolTip(self.ItemVnum)
 
 	def OverInDropItemSlot(self, slotIndex):
-		if self.ToolTipItem:
-			if self.ScrollPos in self.DropDict:
-				if slotIndex in self.DropDict[self.ScrollPos]:
-					self.ToolTipItem.SetItemToolTip(self.DropDict[self.ScrollPos][slotIndex])
+		if self.tooltipitem:
+			if self.CurrentPage in self.DropDict:
+				if slotIndex in self.DropDict[self.CurrentPage]:
+					self.tooltipitem.SetItemToolTip(self.DropDict[self.CurrentPage][slotIndex])
 
 	def OverOutItem(self):
-		if self.ToolTipItem:
-			self.ToolTipItem.HideToolTip()
-			self.ToolTipItem.ClearToolTip()
-
+		if self.tooltipitem:
+			self.tooltipitem.HideToolTip()
+			self.tooltipitem.ClearToolTip()
+	
 	def Close(self):
 		self.OverOutItem()
 		self.Hide()
-
-	def OnScroll(self):
-		self.ScrollPos = int(self.PageCount * self.ScrollBar.GetPos())
-		self.UpdateItems()
-
-	def OnRunMouseWheel(self, pos):
-		if self.ScrollBar.IsShow():
-			if pos > 0:
-				self.ScrollBar.OnUp()
-			else:
-				self.ScrollBar.OnDown()
 
 	def OnPressEscapeKey(self):
 		self.Close()
